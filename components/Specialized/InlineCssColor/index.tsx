bb06b296d024d33681c5b19e200ba95cd324268d
Chasmical <hillankeyxl2@yandex.ru>
2024-12-20 14:22:13 +0700

    ✨ Add InlineCssColor component

---
+++ components/Specialized/InlineCssColor/index.tsx
@@ -0,0 +1,18 @@
+import Code from "@components/Common/Code";
+import styles from "./index.module.scss";
+
+export interface InlineCssColorProps extends React.HTMLAttributes<HTMLElement> {
+  color: string;
+  // ...props
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+export default function InlineCssColor({ color, ...props }: InlineCssColorProps) {
+  return (
+    <Code {...props}>
+      <span className={styles.color} style={{ backgroundColor: color }} />
+      {color}
+    </Code>
+  );
+}
