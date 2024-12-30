e25dda1446b5fa017968adf3ff9691cdc1eaa221
Chasmical <hillankeyxl2@yandex.ru>
2024-12-30 08:17:39 +0700

    🩹 Add viewport page metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { inter, JetBrains_Mono_NL } from "@app/fonts";
 import "./global.scss";
 import clsx from "clsx";
@@ -6,6 +6,11 @@ import clsx from "clsx";
 export const metadata: Metadata = {
   title: "chsm.dev",
   description: "A personal website for a bunch of stupid stuff",
+};
+
+export const viewport: Viewport = {
+  colorScheme: "dark",
+  themeColor: "#fbb946", // var(--color-accent)
 };
 
 export default function RootLayout({ children }: React.PropsWithChildren) {
